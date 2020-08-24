034d2fcef78247d5a23bc3be43b4f3503b0f94d4
BuildTools <unconfigured@null.spigotmc.org>
2020-08-24 21:15:56 +0200

    Initial Commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,20 @@
+const {merge} = require('webpack-merge')
+const commonConfig = require('./webpack.config.common')
+const path = require('path')
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname,'dist'),
+    port: 2020
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
+  }
+})
